70770daa6f867c102eb7084505bb330414a012e5
Lunacie <lunacy.twinsun@gmail.com>
2017-11-29 21:22:16 +0100

    Player class refactor + added Character inheritance

--- js/player.js
+++ js/player.js
@@ -1,11 +1,6 @@
 
 function Player()
 {
-    this.x = 5.00;
-    this.y = 5.00;
-
-    this.xBlock = 5;
-    this.yBlock = 5;
     this.map = {};
 
     this.update = function()
@@ -48,31 +43,6 @@ function Player()
     };
 
 
-    this.draw = function(ox, oy)
-    {
-      ctx.fillStyle = "red";
-      ctx.strokeStyle = "black";
-
-      var x = ox + tiles.size / 2;
-      var y = oy + tiles.size + tiles.size / 20;
-
-      // Player displacement: don't allow if map end
-       if (this.x > 0) {
-        x += (this.x - this.xBlock) * (tiles.size / 2.7);
-        y += (this.x - this.xBlock) * (tiles.size / 4.7);
-      }
-      if (this.y > 0) {
-        x -= (this.y - this.yBlock) * (tiles.size / 2.7);
-        y += (this.y - this.yBlock) * (tiles.size / 4.7);
-      }
-      ctx.beginPath();
-      ctx.moveTo(x, y);
-      ctx.lineTo(x - 20, y + 200);
-      ctx.lineTo(x + 20, y + 200);
-      ctx.closePath();
-      ctx.fill();
-      ctx.stroke();
-    };
 };
 
 Player.prototype = new Character();
