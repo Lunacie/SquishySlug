e142a629faee897481b11f7451601c8fb8b6dee9
Lunacie <lunacy.twinsun@gmail.com>
2017-11-30 00:01:35 +0100

    smoother character movements

--- js/character.js
+++ js/character.js
@@ -35,12 +35,12 @@ function Character ()
 
     // Player displacement: don't allow if map end
      if (this.x > 0) {
-      x += (this.x - this.xBlock) * (tiles.size / 2.7);
-      y += (this.x - this.xBlock) * (tiles.size / 4.7);
+      x += (this.x - this.xBlock) * (tiles.size / 2.14);
+      y += (this.x - this.xBlock) * (tiles.size / 4.14);
     }
     if (this.y > 0) {
-      x -= (this.y - this.yBlock) * (tiles.size / 2.7);
-      y += (this.y - this.yBlock) * (tiles.size / 4.7);
+      x -= (this.y - this.yBlock) * (tiles.size / 2.14);
+      y += (this.y - this.yBlock) * (tiles.size / 4.14);
     }
 
 
