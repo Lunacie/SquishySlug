5274d73ac826a5e2127c4468da4b4005e9f0437a
Lunacie <lunacy.twinsun@gmail.com>
2018-01-24 18:22:03 +0100

    added horizontal auto center

--- js/draw.js
+++ js/draw.js
@@ -8,16 +8,9 @@
 
   var draw = function(player, map, timestamp)
   {
-    //x  += (map.width / 2) * (tiles.size / 4);
-    //y  -= (map.height / 2) * (tiles.size / 4);
     var x = x2d;
     var y = y2d;
-    x += canvas.width / (2);
 
-    if (ratio >= 1)
-      tmp = (map.height * tiles.size) / (3 * ratio);
-    else
-      tmp = (map.height * tiles.size) / (4 / ratio);
     //console.log("map : ", map.width, map.height);
     //console.log("player : ", player.x2d, player.y2d );
     //console.log("canvas : ", canvas.width, canvas.height, canvas.height / 2 );
@@ -27,17 +20,16 @@
     else if (player.y2d < (canvas.height / 2) - 100)
       y += 50;
 
-    var offset = 0;
-      offset -= tiles.size;
-    //console.log("offset : ", offset, (map._startY * -1))
+    if (player.x2d > (canvas.width / 2) + 200)
+      x -= 50;
+    else if (player.x2d < (canvas.width / 2) - 200)
+      x += 50;
 
     y2d = y;
-
+    x2d = x;
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctxOff.clearRect(0, 0, canvas.width, canvas.height);
-    //ctx_char01.clearRect(0, 0, canvas.width, canvas.height);
-    map.draw(x, y - (tiles.size * 2) + offset);
-    //player.draw(x, y);
+    map.draw(x, y - (tiles.size * 3));
     map.drawOverlay();
   }
