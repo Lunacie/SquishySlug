24d7b8cc9bc9e27bf0b6996735be136db2ca0469
Lunacie <lunacy.twinsun@gmail.com>
2018-02-02 00:08:15 +0100

    added 2nd species

--- js/image-loader.js
+++ js/image-loader.js
@@ -15,7 +15,9 @@
        "assets/vectors/char01_up_walk.svg",
      ]
    ];
-    this.speciesStr = [ "BUNNY_", "CAT_", "ELEPHANT_", "INSECT_"];
+    this.speciesStr = [
+      "BUNNY_x5F_", "CAT_x5F_", "ELEPHANT_x5F_", "INSECT_x5F_"
+    ];
     this.elements = [
       "arm_x5F_front_x5F_top",
       "arm_x5F_front_x5F_bot",
@@ -41,7 +43,8 @@
       "nose",
       "ear", "ear_x5F_back", "ear_x5F_front",
       "hair", "hair_x5F_back", "hair_x5F_front",
-      "chest", "top"
+      "chest", "top",
+      "hip_x5F_front", "hip_x5F_back"
     ];
 
      this.load = function (char) {
@@ -53,37 +56,43 @@
        $.get(path, function(svgXml) {
          loader._svgXml = svgXml;
          char.images[char.state][char.direction] = {};
-
          loader._loadOnCanvasImage(char);
          loader._loadOffCanvasImage(char);
        });
      }
 
-     this._loadOffCanvasImage = function(char) {
-
-       if (!char.images.offHexColor)
-         char.images.offHexColor = char.hexColor;
-       char.images[char.state][char.direction].off = new Image();
-       var off = this._serialized.replace(/fill(.*);/g,'fill:#'+char.images.offHexColor.toString(16)+';');
-       char.images[char.state][char.direction].off.src =
-                              "data:image/svg+xml;charset=utf-8," + off;
-
-     }
 
      this._loadOnCanvasImage = function(char) {
 
        char.images[char.state][char.direction].on = new Image();
 
        this._clearDefault();
+       this._normalizeSpecies(char._species);
        this._applySpecies[char._species](this);
+       this._removeOthers(char._species);
+       //if (char.state == ACTION_STATE_WALK)
+         //this._appendWalkAnimations(char.direction);
+
+       //console.log(this._svgXml);
        var str = (new XMLSerializer).serializeToString(this._svgXml);
        this._serialized = str;
        char.images[char.state][char.direction].on.src =
                               "data:image/svg+xml;charset=utf-8," + str;
      }
 
-     this._clearDefault = function() {
+     this._appendWalkAnimations = function(direction) {
+       var strings = ["right", "left", "down", "up"];
+       var path = "assets/animation/animation-";
+       path += strings[direction] + ".css";
+       var loader = this;
+        $.get(path, function(data) {
+          var element = loader._svgXml.getElementsByTagName("style")[0];
+          element.innerHTML += data;
+          //console.log(element.innerHTML);
+        });
+      }
 
+     this._clearDefault = function() {
        for (var i = 0; i < this.elements.length; i++) {
          var element = this._svgXml.getElementById(this.elements[i]);
          if (element)
@@ -91,11 +100,48 @@
        }
        for (var i = 0; i < this.addons.length; i++) {
          var element = this._svgXml.getElementById(this.addons[i]);
-         if (element)
-           element.id = this.speciesStr[0] + this.elements[i];
+         if (element) {
+           element.id = this.speciesStr[0] + this.addons[i];
+           console.log(element);
+
+         }
        }
-       console.log(this._svgXml);
      }
+
+     this._normalizeSpecies = function(species) {
+       for (var i = 0; i < this.elements.length; i++) {
+         var id = this.speciesStr[species] + this.elements[i];
+         var element = this._svgXml.getElementById(id);
+         if (element)
+           element.id = this.elements[i];
+       }
+       for (var i = 0; i < this.addons.length; i++) {
+         var id = this.speciesStr[species] + this.addons[i];
+         var element = this._svgXml.getElementById(id);
+         if (element)
+           element.id = this.addons[i];
+       }
+     }
+
+
+      this._removeOthers = function(current) {
+        for (var i = 0; i < this.elements.length; i++) {
+          for (var j = 0; j < this.speciesStr.length; j++) {
+            var id = this.speciesStr[j] + this.elements[i];
+            var element = this._svgXml.getElementById(id);
+            if (element)
+              element.remove();
+          }
+        }
+        for (var i = 0; i < this.addons.length; i++) {
+          for (var j = 0; j < this.speciesStr.length; j++) {
+            var id = this.speciesStr[j] + this.addons[i];
+            var element = this._svgXml.getElementById(id);
+            if (element)
+              element.remove();
+          }
+        }
+      }
 
      this._applySpecies = [
        // bunny
@@ -103,6 +149,35 @@
        },
        // cat
        function(loader)  {
+         console.log(loader._svgXml);
+         var shared = [
+           // from bunny
+           [ "head", "neck", "chest", "nose",
+            "whisker_x5F_left_x5F_top",
+            "whisker_x5F_left_x5F_mid",
+            "whisker_x5F_left_x5F_bot",
+            "whisker_x5F_right_x5F_top",
+            "whisker_x5F_right_x5F_mid",
+            "whisker_x5F_right_x5F_bot",
+            "arm_x5F_front_x5F_top",
+            "arm_x5F_front_x5F_bot",
+            "arm_x5F_back_x5F_top",
+            "arm_x5F_back_x5F_bot",
+            "ear", "ear_x5F_back", "ear_x5F_front",
+            "foot_x5F_front",
+            "foot_x5F_back",
+          ]
+         ]
+         for (var i = 0; i < shared.length; i++) {
+           for (var j = 0; j < shared[i].length; j++) {
+              var id = loader.speciesStr[i] + shared[i][j];
+              var element = loader._svgXml.getElementById(id);
+              if (element)
+                element.id = shared[i][j];
+              //console.log(element);
+           }
+         }
+
        },
        // elephant
        function(loader)  {
@@ -112,5 +187,16 @@
        },
      ]
 
+     this._loadOffCanvasImage = function(char) {
+
+       if (!char.images.offHexColor)
+         char.images.offHexColor = char.hexColor;
+       char.images[char.state][char.direction].off = new Image();
+       var off = this._serialized.replace(/fill(.*);/g,'fill:#' +
+                 char.images.offHexColor.toString(16) + ';');
+       char.images[char.state][char.direction].off.src =
+                              "data:image/svg+xml;charset=utf-8," + off;
+
+     }
 
  }
