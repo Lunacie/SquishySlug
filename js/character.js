305ccfcae809764bf6cb5f6f1c0efe0e698d17eb
Lunacie <lunacy.twinsun@gmail.com>
2018-09-17 19:20:55 +0200

    restoring walk pace

--- js/character.js
+++ js/character.js
@@ -27,8 +27,8 @@ function Character (x, y)
   };
   this.direction = 0;
   this.images = [];
-  this.walkUnitSize = 0.015;
-  //this.walkUnitSize = 0.025;
+  //this.walkUnitSize = 0.015;
+  this.walkUnitSize = 0.025;
   //this.walkUnitSize = 0.045;
   this._walkPaceScale = 1;
   this.walkUnit = {
